e4f373a767dcd94fecbcd1d8577f7d9ed7a07413
James Royer <walkingalchemy@gmail.com>
2017-10-27 23:22:57 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash == {}
+    return nil
+  end
+  ages = []
+  names = []
+  name_hash.each do |name, age|
+    ages.push(age)
+    names.push(name)
+  end
+  names[ages.index(ages.min)]
+
 
 end
