db3cea3a81aed37809f00955f568aea80bcc6717
James Royer <walkingalchemy@gmail.com>
2017-10-27 23:14:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   ages = []
   names = []
-  name_hash.each { |name, age| ages.push(age) names.push(name) }
+  name_hash.each { |name, age| ages.push(age); names.push(name) }
   names[find_index(ages.min)]
 
 
