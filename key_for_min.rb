d7f5b598734986b97a10936d8e3c763c55f18b53
James Royer <walkingalchemy@gmail.com>
2017-10-27 23:10:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect {|name, age| age}.min
+  ages = []
+  names = []
+  name_hash.each { |name, age| ages.push(age) names.push(name) }
+  names[find_index(ages.min)]
+
+
 end
